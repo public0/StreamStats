0ec0322d16a56fa61b80b0d03579423146ff4639
Ivan Alex <alexandru.ivan.daniel@gmail.com>
2021-12-10 10:03:22 +0200

    rollback fix

--- database/migrations/2021_12_07_072107_create_streams_table.php
+++ database/migrations/2021_12_07_072107_create_streams_table.php
@@ -41,6 +41,8 @@ class CreateStreamsTable extends Migration
      */
     public function down()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         Schema::dropIfExists('streams');
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
 }
